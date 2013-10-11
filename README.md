2fcafa74eb4003f7c072d35781316d68cde0d445
Mike Kibbel <mkibbel@gmail.com>
2013-10-11 16:43:54 -0400

    Set up project skeleton.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ django-commando
 ===============
 
 A declarative API for writing, overriding and sequencing Django management commands
+
+### Work in progress
+
+This project is in early development. Stay tuned for progress.
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+import commando
+
+
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as source:
+        return source.read()
+
+
+setup(
+    name="django-grunt",
+    version=commando.__version__,
+    description="A declarative API for writing, overriding and sequencing Django management commands",
+    long_description=read("README"),
+    author="Mike Kibbel",
+    author_email="mkibbel@gmail.com",
+    url="https://github.com/skibblenybbles/django-commando",
+    license="MIT License",
+    platforms=["OS Independent"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    packages=find_packages(exclude=["example", "example.*"]),
+    install_requires=[
+        "Django>=1.4",
+    ],
+)
